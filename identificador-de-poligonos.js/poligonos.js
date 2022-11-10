35484a7ccaf88eb09c270de6d4bce702a24b9de5
Mah <marinalarissacarpesrohrig@gmail.com>
2022-11-10 20:18:11 -0300

    exercicio 2

--- identificador-de-poligonos.js/poligonos.js
+++ identificador-de-poligonos.js/poligonos.js
@@ -11,22 +11,20 @@ if (qtdLados < 3){
     let semiPerimetro = ((medidaLado1+medidaLado2+medidaLado3)/2);
     let area = Math.sqrt(semiPerimetro*(semiPerimetro-medidaLado1)*(semiPerimetro-medidaLado2)*
                         (semiPerimetro-medidaLado3));
-    console.log("A figura é um triângulo e sua área é: "+area.toPrecision(4))
+    console.log("A figura é um triângulo e sua área é: "+area)
 
 
     //triangulo
 }else if(qtdLados==4){
-    let medidaLado1=2;
-    let medidaLado2=2;
-    let medidaLado3=2;
-    let medidaLado4=2;
+    let medidaComprimento=2;
+    let medidaAltura=2;
+ console.log("A figura é um quadrado e sua área é :" +(medidaAltura*medidaComprimento))
     //quadrado
 }else if(qtdLados==5){
-    let medidaLado1=2;
-    let medidaLado2=2;
-    let medidaLado3=2;
-    let medidaLado4=2;
-    let medidaLado5=2;
+    let perimetro =2;
+    let apotema=2;
+
+    console.log("A fica é um pentágono e sua área é: " +(perimetro*apotema))
     //pentágono
 }
 
