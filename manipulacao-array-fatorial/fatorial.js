eff83e715d8a2f6f5b5308efd78830b75bff8192
Mah <marinalarissacarpesrohrig@gmail.com>
2022-11-10 21:56:24 -0300

    js que inverte palavra digitada e calculo de fatorial

---
+++ manipulacao-array-fatorial/fatorial.js
@@ -0,0 +1,14 @@
+//Calcula fatorial do número na variável x
+
+let x = 5;
+let result = x;
+
+
+// laço para calcular o multiplicador
+for (let i = 1; i < x; i++) { 
+    result = result*i; 
+}
+
+console.log(result);
+
+
---
+++ manipulacao-array-fatorial/manipula-array.js
@@ -0,0 +1,11 @@
+// Inverte a palavra da variável word e imprime no console
+
+
+var word="marina";
+var drow="";
+
+for (var i=word.length; i>=0; i--){
+    var drow = drow + word.charAt(i);
+}
+
+console.log(drow);
