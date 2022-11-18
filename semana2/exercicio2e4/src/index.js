01a91c32b76d0669051843e9b5f4885b41467a57
Mah <marinalarissacarpesrohrig@gmail.com>
2022-11-17 22:05:24 -0300

    exercicio 2 e 4 da semana 2

---
+++ semana2/exercicio2e4/src/index.js
@@ -0,0 +1,36 @@
+// import diminuir from "./maisUm.js";
+// const mensagemSucesso = 'funcionou'
+// console.log(mensagemSucesso)
+
+// function soma(a, b) {
+//     return (a + b);
+
+// }
+
+// console.log(soma(2, 3));
+
+// console.log(diminuir(5,3));
+
+// import * as mod from './util/util.js';
+// // imprime as funções
+// //console.log (mod);
+
+// // configura para moeda
+// console.log(mod.currencyFormat(28888883));
+// // adiciona 0(x) antes do número informado(y)
+// console.log(mod.addLeadingZeros(22222,10));
+//  //adiciona 0 se não for um número válido de cpf inteiro 
+// console.log(mod.convert_to_cpf('2039'));
+// //converte para cpf , exige string, ajusta a pontuação
+// console.log(mod.cpfMask('03707984060'));
+// //organiza o array 
+// let array=[1,2,5,42,6,1,10];
+// console.log(mod.sortArray(array));
+
+//document.body.innerText = 'Inner Text';
+
+// aqui pede uma requisição da página da wikipedia
+// async informa que tem um tempo de espera ( do await) antes de funcionar a função 
+// Exercicio 4:
+
+
---
+++ semana2/exercicio2e4/src/maisUm.js
@@ -0,0 +1,5 @@
+function diminuir(value1,value2){
+    return value1,value2;
+}
+
+export default diminuir;
